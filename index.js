e26576fce55647a6a0ae0e056b4c0403e1264ede
Kim Love <kimberlymlove15@gmail.com>
2023-08-06 16:00:24 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(driversArray, searchName){
+    const filteredNames = driversArray.filter((name) => {
+        return (name === searchName || name.toLowerCase() === searchName.toLowerCase())
+    })
+
+    return filteredNames
+}
+
+function fuzzyMatch(driversArray, searchValue){
+    const filteredNames = driversArray.filter((name) => {
+        return (searchValue[0] === name[0] && searchValue[1] === name[1])
+    })
+
+    return filteredNames
+}
+
+function matchName(driversArray, searchName){
+    const filteredNames = driversArray.filter((driverObj) => {
+        return (driverObj.name === searchName)
+    })
+
+    return filteredNames
+}
